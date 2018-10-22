b3903526a11a051ed9b00a3b259ad3ef972b68d6
Majiyagbe Oluwole <oluwole564@gmail.com>
2018-10-22 16:42:52 +0100

    Mysql Port

--- config/database.js
+++ config/database.js
@@ -51,7 +51,7 @@ module.exports = {
     client: 'mysql',
     connection: {
       host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
+      port: Env.get('DB_PORT', '3306'),
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', 'emeraldberyl9764eba'),
       database: Env.get('DB_DATABASE', 'MassMailer')
